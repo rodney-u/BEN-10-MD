0c272edea082990c6de56fce6e5233b5294f3f34
rodney-u <rodneyraph4@gmail.com>
2025-08-06 22:47:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: '',
-  author: '‎',
-  botName: ".",
-  botOwner: '103', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
+  author: 'RAPH‎',
+  botName: "BEN 10",
+  botOwner: 'RODNEY', // Your name
+  ownerNumber: '254745314237', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
